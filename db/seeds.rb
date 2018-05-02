3b4bc3338835da30db8bda42dd4cee67f8a9ab10
Astm <astm@Astm-MacBook.local>
2018-05-02 13:35:19 +0300

    read me file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 
 # Create admin user
-# User.create(first_name: 'Astm', last_name: 'Ali', email: 'astm@astm.com',  password: 'astmastm', password_confirmation: 'astmastm')
+User.create(first_name: 'Astm', last_name: 'Ali', email: 'astm@astm.com',  password: '123456', password_confirmation: 'astmastm')
